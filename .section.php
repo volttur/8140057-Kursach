be8acf91a95091347d84f51ef66163cc59a7a6ef
Artur Aristov <volttur@yandex.ru>
2017-01-14 08:17:10 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем засесть здесь надолго",
+   "keywords" => "много игр, фана",
+   "robots" => "index, follow"
+);
+?>
---
+++ include/company_name.php
@@ -0,0 +1,1 @@
+RatHole
